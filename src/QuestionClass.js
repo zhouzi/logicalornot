1c9e3842c499ffeb91cf0fbc6473093958f64fc4
Gabin <hello@gabinaureche.com>
2015-04-26 16:44:52 +0200

    [refactor] use questions as a json file

--- src/QuestionClass.js
+++ src/QuestionClass.js
@@ -1,9 +1,8 @@
-import questions from './questions';
+import questions from './questions.json';
 import rand from './rand';
 
 export default class QuestionClass {
   constructor () {
-    this.questions = questions;
     this.clear();
   }
 
@@ -16,20 +15,20 @@ export default class QuestionClass {
     this.pickedIndexes.push(index);
     this.currentIndex = index;
 
-    return this.questions[index];
+    return questions[index];
   }
 
   get currentQuestion () {
     if (this.currentIndex === null) return {};
-    return this.questions[this.currentIndex];
+    return questions[this.currentIndex];
   }
 
   get randomQuestion () {
-    let index = rand(0, this.questions.length - 1);
+    let index = rand(0, questions.length - 1);
     let i = 0;
 
     while (this.pickedIndexes.indexOf(index) >= 0 && ++i < 10) {
-      index = rand(0, this.questions.length - 1);
+      index = rand(0, questions.length - 1);
     }
 
     return this.getQuestion(index);
