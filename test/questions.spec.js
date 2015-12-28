2a6d1b0f5ca5974b6b377b610fa04d343d5ba752
Gabin <hello@gabinaureche.com>
2015-12-28 22:36:57 +0400

    update questions spec

--- test/questions.spec.js
+++ test/questions.spec.js
@@ -3,12 +3,14 @@
 import evalToString from '../src/app/utils/evalToString'
 import questions from '../src/app/data/questions.json'
 
-it('should have 1 question with a list of 3 answers, the right one included', () => {
-  questions.forEach(function (q) {
-    expect(typeof q.question).toBe('string')
-    expect(q.answers.length).toBe(3)
+describe('questions', function () {
+  it('should have a question with a list of 3 answers (the right one included)', () => {
+    questions.forEach(function (q) {
+      expect(typeof q.question).toBe('string')
+      expect(q.answers.length).toBe(3)
 
-    const rightAnswer = evalToString(q.question)
-    expect(q.answers.some(answer => evalToString(answer) === rightAnswer)).toBe(true)
+      const rightAnswer = evalToString(q.question)
+      expect(q.answers.some(answer => evalToString(answer) === rightAnswer)).toBe(true)
+    })
   })
 })
