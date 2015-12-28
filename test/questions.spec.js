01a772bfba680cc409956d3f75259c6a3fa2436a
Gabin <hello@gabinaureche.com>
2015-12-28 22:18:15 +0400

    remove custom matcher

--- test/questions.spec.js
+++ test/questions.spec.js
@@ -1,17 +1,14 @@
-/* global jasmine, beforeEach, it, expect */
+/* global it, expect */
 
 import evalToString from '../src/app/utils/evalToString'
 import questions from '../src/app/data/questions.json'
-import matchers from './matchers'
-
-beforeEach(function () {
-  jasmine.addMatchers(matchers)
-})
 
 it('should have 1 question with a list of 3 answers, the right one included', () => {
   questions.forEach(function (q) {
     expect(typeof q.question).toBe('string')
     expect(q.answers.length).toBe(3)
-    expect(q.answers).toContainAnswer(evalToString(q.question))
+
+    const rightAnswer = evalToString(q.question)
+    expect(q.answers.some(answer => evalToString(answer) === rightAnswer)).toBe(true)
   })
 })
